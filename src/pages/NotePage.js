1e39399be4bf828c5af4b9756b38cff43ec2ee79
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2022-07-06 22:43:37 +0530

    single note renders but not functional

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,12 +1,24 @@
-import React from "react";
-import notes from "../assests/data";
+import React, { useEffect, useState } from "react";
+// import notes from "../assests/data"; - no longer using this file.
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { ReactComponent as ArrowLeft } from "../assests/arrow-left.svg";
 
 const NotePage = () => {
   const { id } = useParams();
-  const specificNote = notes.find((specific) => specific.id === Number(id)); //filter for matching note with specific id
+  //  const specificNote = notes.find((specific) => specific.id === Number(id)); - filter for matching note with specific id
+  let [specificNote, setSpecificNote] = useState(null);
+
+  useEffect(() => {
+    getNotes();
+  }, [id]);
+
+  let getNotes = async () => {
+    let response = await fetch(`http://localhost:8000/notes/${id}`);
+    let data = await response.json();
+    setSpecificNote(data);
+  };
+
   return (
     <div className="note">
       <div className="note-header">
--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -1,8 +1,20 @@
-import React from "react";
-import notes from "../assests/data";
+import React, { useEffect, useState } from "react";
+//import notes from "../assests/data"; - no longer using this file.
 import ListItem from "../components/ListItem";
 
 const NotesListPages = () => {
+  let [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  let getNotes = async () => {
+    let response = await fetch("http://localhost:8000/notes/");
+    let data = await response.json();
+    setNotes(data);
+  };
+
   return (
     <div className="notes">
       <div className="notes-header">
