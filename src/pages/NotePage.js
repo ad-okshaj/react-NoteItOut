0258ca79192874ae992b00be3ca9127d75cc4ee9
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2022-07-07 10:37:35 +0530

    fixed single note to notes list routing
    use chrome browser + no extensions

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,20 +1,21 @@
 // import notes from "../assests/data"; - no longer using this file.
-import React, { useEffect, useState, useNavigate } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { ReactComponent as ArrowLeft } from "../assests/arrow-left.svg";
 
 const NotePage = () => {
-  const { id } = useParams();
-  //  const specificNote = notes.find((specific) => specific.id === Number(id)); - filter for matching note with specific id
-  // let navigate = useNavigate();
+  let navigate = useNavigate();
+  let { id } = useParams();
+  //  const specificNote = notes.find((specific) => specific.id === Number(id)); //filter for matching note with specific id
   let [specificNote, setSpecificNote] = useState(null);
 
   useEffect(() => {
-    getNotes();
-  }, [id]);
+    getNote();
+  }, []);
 
-  let getNotes = async () => {
+  let getNote = async () => {
     let response = await fetch(`http://localhost:8000/notes/${id}`);
     let data = await response.json();
     setSpecificNote(data);
@@ -30,17 +31,17 @@ const NotePage = () => {
     });
   };
 
-  // let handleSubmit = () => {
-  //   updateNote();
-  //   navigate("/");
-  // };
+  let handleSubmit = () => {
+    updateNote();
+    navigate(-1);
+  };
 
   return (
     <div className="note">
       <div className="note-header">
         <h3>
           <Link to="/">
-            <ArrowLeft />
+            <ArrowLeft onClick={handleSubmit} />
           </Link>
         </h3>
       </div>
