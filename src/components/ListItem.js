051b181a2e1a867cc1c107790ab3e07cfd6888ba
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2022-07-07 12:41:54 +0530

    generate title and body automatically.

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+let getDate = (noteProp) => {
+  return new Date(noteProp.updated).toLocaleDateString();
+};
+
+let getTitle = (noteProp) => {
+  const title = noteProp.body.split("\n")[0];
+  if (title.length > 40) {
+    return title.slice(0, 40);
+  }
+  return title;
+};
+
+let getContent = (noteProp) => {
+  let title = getTitle(noteProp);
+  let content = noteProp.body.replaceAll("\n", " ");
+  content = content.replaceAll(title, "");
+
+  if (content.length > 45) {
+    return content.slice(0, 45);
+  } else {
+    return content;
+  }
+};
+
 const ListItem = ({ noteProp }) => {
   return (
     <Link to={`/note/${noteProp.id}`}>
       <div className="notes-list-item">
-        <h3>{noteProp.body}</h3>
+        <h3>{getTitle(noteProp)}</h3>
+        <p>
+          <span>{getDate(noteProp)}</span>
+          {getContent(noteProp)}
+        </p>
       </div>
     </Link>
   );
