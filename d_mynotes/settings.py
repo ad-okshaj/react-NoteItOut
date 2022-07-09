982af1c7fbaa93a7639244dd2c1d019c04c51bf0
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2022-07-09 17:37:58 +0530

    BrowserRouter ----> HashRouter

--- d_mynotes/settings.py
+++ d_mynotes/settings.py
@@ -25,13 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'api.apps.ApiConfig',
     'rest_framework',
-
     "corsheaders",
-
-
 
 ]
 
@@ -52,7 +48,9 @@ ROOT_URLCONF = 'd_mynotes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'frontend/build'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,6 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'frontend/build/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import Header from "./components/Header";
 import NotesListPage from "./pages/NotesListPage"; // imp shortcut
 import NotePage from "./pages/NotePage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
