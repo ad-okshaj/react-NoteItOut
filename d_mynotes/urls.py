f2a9c68409db5db378b330db229608533f4377e3
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2022-09-07 21:45:59 +0530

    ignore this commit

--- d_mynotes/urls.py
+++ d_mynotes/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # takes care of all files starting with ''
     path('api/', include('api.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
+    path('test/', admin.site.urls),
+
 ]
