04ddf5fa607b0123e0484bc07be8326ad92c64e8
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2022-09-08 11:54:24 +0530

    fixing re-rendering isssue

--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
-//import notes from "../assests/data"; - no longer using this file.
 import ListItem from "../components/ListItem";
 import AddButton from "../components/AddButton";
-import { Link } from "react-router-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const NotesListPages = () => {
   let [notes, setNotes] = useState([]);
@@ -19,21 +16,19 @@ const NotesListPages = () => {
   };
 
   return (
-    <>
-      <div className="notes">
-        <div className="notes-header">
-          <h2 className="notes-title">&#9782; Notes</h2>
-          <a href="/logout">LOGOUT</a>
-          <p className="notes-count">{notes.length}</p>
-        </div>
-        <div className="notes-list">
-          {notes.map((noteObj, index) => (
-            <ListItem key={index} noteProp={noteObj} />
-          ))}
-        </div>
-        <AddButton />
+    <div className="notes">
+      <div className="notes-header">
+        <h2 className="notes-title">&#9782; Notes</h2>
+        <a href="/logout">LOGOUT</a>
+        <p className="notes-count">{notes.length}</p>
       </div>
-    </>
+      <div className="notes-list">
+        {notes.map((noteObj, index) => (
+          <ListItem key={index} noteProp={noteObj} />
+        ))}
+      </div>
+      <AddButton />
+    </div>
   );
 };
 
